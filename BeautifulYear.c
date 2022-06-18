5008cdf7e0cb7566848ec56223468db1cddafa05
Rizo11 <maribjonov2002@gmail.com>
2022-06-19 01:17:23 +0500

    brute force

--- BeautifulYear.c
+++ BeautifulYear.c
@@ -1,15 +1,37 @@
+// https://codeforces.com/problemset/problem/271/A
+
 #include <stdio.h>
+
+_Bool isBeautifulYear(int year)
+{
+    _Bool nums[10] = {0};
+    if(!nums[year%10])
+        nums[year%10] = 1;
+    if(!nums[(year/10)%10])
+        nums[(year/10)%10] = 1;
+    else
+        return 0;
+    if(!nums[(year/100)%10])
+        nums[(year/100)%10] = 1;
+    else
+        return 0;
+    if(!nums[(year/1000)%10])
+        nums[(year/1000)%10] = 1;
+    else
+        return 0;
+    return 1;
+}
 
 int main()
 {
     short year = 0;
     scanf("%d", &year);
-    _Bool nums[10] = {0};
-    nums[year%10] = 1;
-    // 2016
-    nums[(year/10)%10] = 1;
-    nums[(year/100)%10] = 1;
-    nums[(year/1000)%10] = 1;
-    int newNum = 0;
-    
+    short newYear = year;
+    _Bool isBeautiful = 0;
+    while (!isBeautiful)
+    {
+        newYear++;
+        isBeautiful = isBeautifulYear(newYear);
+    }
+    printf("%d\n", newYear);
 }
