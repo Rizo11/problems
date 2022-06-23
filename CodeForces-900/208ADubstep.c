18638e878606eba2f9614eda43e69e1170846757
Rizo11 <maribjonov2002@gmail.com>
2022-06-23 23:57:23 +0500

    strings

--- CodeForces-900/208ADubstep.c
+++ CodeForces-900/208ADubstep.c
@@ -4,16 +4,33 @@
 
 int main()
 {
-    char song[200] = {'0'};
-    scanf("%s", song);
-    for (short i = 0; i < 198; i++)
+    char song[202] = {'0'};
+    short isSpace = 0, length = 0;
+    char a;
+    scanf("%c", &a);
+    while(a != '\n')
     {
-        if(song[i] != 'W' && song[i+1] != 'U' && song[i+2] != 'B')
+        song[length] = a;
+        length++;
+        scanf("%c", &a);
+    }
+    for (short i = 0; i < length; i++)
+    {
+        if(!(song[i] == 'W' && song[i+1] == 'U' && song[i+2] == 'B'))
         {
+            if (isSpace)
+            {
+                printf(" ");
+                isSpace = 0;
+            }
             printf("%c", song[i]);
         }
         else
+        {
+            isSpace = 1;
             i += 2;
+        }
     }
-    
 }
+
+// WUBUUA
